457666a96b757d6d99627c610ac421a581761fab
Richard William Mann <richard.mann@maine.edu>
2021-04-25 16:46:50 -0400

    sure

--- readtemp.c
+++ readtemp.c
@@ -7,6 +7,7 @@
 #define F_CPU 8000000UL
 #include <util/delay.h>
 #include <string.h>
+#include <stdlib.h>
 
 int serial_putchar(char, FILE *);
 int serial_getchar(FILE *);
@@ -14,90 +15,104 @@ static FILE serial_stream = FDEV_SETUP_STREAM (serial_putchar, serial_getchar, _
 
 void init_serial(void);
 void init_adc(void);
-int read_adc(void);
+unsigned int read_adc(void);
 
 void update_clock_speed(void);
 
 
 int main()
-{ char buffer[100]="notStart";
-  update_clock_speed();  //adjust OSCCAL
-  init_serial(); 
-  init_adc();  
-  _delay_ms(1000); //let serial work itself out
-//  while(strncmp("Start",buffer,strlen("Start"))!=0) fgets(buffer,100,stdin);
-  while(1) //raspberry pi controls reset line
-  {
-    printf("%d\n",read_adc());
-  }    
+{ 
+	int i;
+	float values[100];
+	int value_len = 0;
+	int current_value = 0;
+	char buffer[100]="notStart";
+	float estimate;
+	char num_buf[8];
+	float sum;
+	unsigned char startbuf[6];
+	update_clock_speed();  //adjust OSCCAL
+        init_serial();
+        init_adc();
+	_delay_ms(1000); //let serial work itself out
+
+	while(1) //raspberry pi controls reset line
+	{
+		estimate = read_adc();
+		dtostrf(estimate, 5, 0, num_buf);
+		printf("%s\n", num_buf);
+	}
 }
 
 
 //read the first two bytes of eeprom, if they have been programmed
-//use the first byte as an offset for adjusting OSCCAL and the second as
-//the direction to adjust 0=increase, 1=decrease.
-//Any offset is allowed and users are cautioned that it is possible to
-// adjust the oscillator beyond safe operating bounds.
+////use the first byte as an offset for adjusting OSCCAL and the second as
+////the direction to adjust 0=increase, 1=decrease.
+////Any offset is allowed and users are cautioned that it is possible to
+//// adjust the oscillator beyond safe operating bounds.
+
 void update_clock_speed(void)
 {
-  char temp;
-  temp=eeprom_read_byte((void *)1); //read oscillator offset sign 
-                                    //0 is positive 1 is  negative
-                                    //erased reads as ff (so avoid that)
-  if(temp==0||temp==1)      //if sign is invalid, don't change oscillator
-  {
-      if(temp==0)
-          {
-             temp=eeprom_read_byte((void *)0);
-                 if(temp != 0xff) OSCCAL+=temp;
-          }
-          else
-          {
-             temp=eeprom_read_byte((void *)0);
-                 if(temp!=0xff) OSCCAL -=temp;
-          }
-  }
+	char temp;
+	temp=eeprom_read_byte((void *)1); //read oscillator offset sign 
+
+
+	if(temp==0||temp==1)      //if sign is invalid, don't change oscillator
+	{
+		if(temp==0)
+		{
+			temp=eeprom_read_byte((void *)0);
+			if(temp != 0xff) OSCCAL+=temp;
+		}
+		else
+		{
+			temp=eeprom_read_byte((void *)0);
+			if(temp!=0xff) OSCCAL -=temp;
+		}
+	}
 }
+
+
+
 
 /* Initializes AVR USART for 9600 baud (assuming 8MHz clock) */
 /* 8MHz/(16*(51+1)) = 9615 about 0.2% error                  */
 void init_serial(void)
 {
-   UBRR0H=0;
-   UBRR0L=51; // 9600 BAUD FOR 1MHZ SYSTEM CLOCK
-   UCSR0A=0;
-   UCSR0C= (1<<USBS0)|(3<<UCSZ00) ;  // 8 BIT NO PARITY 2 STOP
-   UCSR0B=(1<<RXEN0)|(1<<TXEN0)  ; //ENABLE TX AND RX ALSO 8 BIT
-   stdin=&serial_stream;
-   stdout=&serial_stream;
-
+	UBRR0H=0;
+	UBRR0L= 51;
+	UCSR0A=0;
+	UCSR0C= (1<<USBS0)|(3<<UCSZ00) ;  // 8 BIT NO PARITY 2 STOP
+	UCSR0B=(1<<RXEN0)|(1<<TXEN0)  ; //ENABLE TX AND RX ALSO 8 BIT
+	stdin=&serial_stream;
+	stdout=&serial_stream;
 }   
+
 //simplest possible putchar, waits until UDR is empty and puts character
 int serial_putchar(char val, FILE * fp)
 {
-  while((UCSR0A&(1<<UDRE0)) == 0); //wait until empty 
-   UDR0 = val;
-   return 0;
+	while((UCSR0A&(1<<UDRE0)) == 0); //wait until empty 
+	UDR0 = val;
+	return 0;
 }
-
-//simplest possible getchar, waits until a char is available and reads it
-//note:1) it is a blocking read (will wait forever for a char)
-//note:2) if multiple characters come in and are not read, they will be lost
 int serial_getchar(FILE * fp)
 {
-   while((UCSR0A&(1<<RXC0)) == 0);  //WAIT FOR CHAR
-   return UDR0;
-}     
+	while(!(UCSR0A&(1<<RXC0)));  //WAIT FOR CHAR
+	return UDR0;
+}
 void init_adc(void)
 {
-	ADMUX = (3<<REFS0) | 8; //temperature sensor 1.1V ref
+	ADMUX = (3<<REFS0) | 8; //Refference voltage is 1.1V
 	ADCSRA = (1<<ADEN) | (6<<ADPS0); // enable ADC, prescaler=64
 	ADCSRB = 0;
 	DIDR0 = 0;
-} 
-int read_adc(void)
-{
-    ADCSRA |= (1<<ADSC);
-    while(ADCSRA & (1<<ADSC)); //wait for coversion
-    return ADC;
 }
+
+//Read ADC pin and returning the value
+unsigned int read_adc(void)
+{
+	ADCSRA |= (1<<ADSC);
+        while(ADCSRA & (1<<ADSC)); //wait for coversion
+        return ADC;
+}
+
